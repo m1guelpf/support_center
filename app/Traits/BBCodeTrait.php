7cc2e47ab81f4f5c718d31253301398a7618c9fa
Miguel Piedrafita <github@miguelpiedrafita.com>
2017-02-11 17:26:20 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- app/Traits/BBCodeTrait.php
+++ app/Traits/BBCodeTrait.php
@@ -2,11 +2,12 @@
 
 namespace App\Traits;
 
-trait BBCodeTrait{
-public function bbcode($string)
+trait BBCodeTrait
+{
+    public function bbcode($string)
     {
         if (!empty($string)) {
-            $search = array(
+            $search = [
                 '@\[(?i)b\](.*?)\[/(?i)b\]@si',
                 '@\[(?i)i\](.*?)\[/(?i)i\]@si',
                 '@\[(?i)u\](.*?)\[/(?i)u\]@si',
@@ -16,8 +17,8 @@ public function bbcode($string)
                 '@\[(?i)p\](.*?)\[/(?i)p\]@si',
                 '@\[(?i)br/\]@si',
                 '@\[(?i)url=(.*?)\](.*?)\[/(?i)url\]@si',
-            );
-            $replace = array(
+            ];
+            $replace = [
                 '<b>\\1</b>',
                 '<i>\\1</i>',
                 '<u>\\1</u>',
@@ -26,10 +27,12 @@ public function bbcode($string)
                 '<iframe src="http://player.vimeo.com/video/\\1?title=0&amp;byline=0&amp;portrait=0" webkitallowfullscreen="" mozallowfullscreen="" allowfullscreen="" frameborder="0" width="400" height="250"></iframe>',
                 '<p>\\1</p>',
                 '<br/>',
-                '<a href="\\1">\\2</a>'
-            );
-            return preg_replace($search , $replace, $string);
+                '<a href="\\1">\\2</a>',
+            ];
+
+            return preg_replace($search, $replace, $string);
         }
+
         return false;
     }
 }
